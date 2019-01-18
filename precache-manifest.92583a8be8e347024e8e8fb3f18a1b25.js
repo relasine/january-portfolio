d0d4a3ad3e613e7623601a71295273ccede37e2f
Kevin Simpson <relasine@gmail.com>
2019-01-18 12:47:56 -0700

    Updates

---
+++ precache-manifest.92583a8be8e347024e8e8fb3f18a1b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd7826622787dd349b7",
+    "url": "/january-portfolio/static/css/main.dbaa6e6e.chunk.css"
+  },
+  {
+    "revision": "bbd7826622787dd349b7",
+    "url": "/january-portfolio/static/js/main.bbd78266.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/january-portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "c770fcce8685f1c9df94",
+    "url": "/january-portfolio/static/js/runtime~main.c770fcce.js"
+  },
+  {
+    "revision": "b083a2d108ddb31447a94d160e04ad96",
+    "url": "/january-portfolio/static/media/clean-desk.b083a2d1.jpg"
+  },
+  {
+    "revision": "1106f088216ff685d0109236613fb55e",
+    "url": "/january-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github.io/january-portfolio/precache-manifest.d932aa1d0c9e65dfeda69ee3a4db0294.js"
+  "/january-portfolio/precache-manifest.92583a8be8e347024e8e8fb3f18a1b25.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/github.io/january-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/january-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
