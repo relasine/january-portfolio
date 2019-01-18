b7e6941202d28197acc7f143dd8e975687075e24
Kevin Simpson <relasine@gmail.com>
2019-01-18 10:52:10 -0700

    Import Portfolio Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 
 import Header from "../Header/Header";
 import About from "../About/About";
+import Portfolio from "../Portfolio/Portfolio";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <About />
+        <Portfolio />
       </div>
     );
   }
