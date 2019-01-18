77af55cb7aefd05cd3254b4c68cc429264aae253
Kevin Simpson <relasine@gmail.com>
2019-01-18 09:38:00 -0700

    Import About component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "./App.scss";
 
 import Header from "../Header/Header";
+import About from "../About/About";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <About />
       </div>
     );
   }
