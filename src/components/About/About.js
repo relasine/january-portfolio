8cd62ad19e68276509298c0dabd3dd9be64b4447
Kevin Simpson <relasine@gmail.com>
2019-01-18 10:19:17 -0700

    Move about content into a helper file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import "./About.scss";
 
+import { about } from "../../content/about";
+
 class About extends Component {
   render() {
+    const details = about.map(point => {
+      return (
+        <p>
+          {point} <br />
+          <br />
+        </p>
+      );
+    });
     return (
       <section className="about-section">
         <article className="about-hero">
@@ -14,19 +24,7 @@ class About extends Component {
           />
         </article>
         <article className="about-details">
-          <p>
-            I'm Kevin Simpson, a front end developer living in the north Denver
-            metro area.
-            <br />
-            <br />I am tenacious problem solver with a passion for React, CSS,
-            unit testing, and teaching. I push myself to learn something new
-            about programming every day, and I take criticism with humility and
-            grace.
-            <br />
-            <br />
-            In my free time I am a husband and father who enjoys chasing my son
-            around the house, Mario Kart marathons, and 90s-era JRPGs.
-          </p>
+          <div>{details}</div>
         </article>
       </section>
     );
