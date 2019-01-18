7a659169d5e5f83918919453db09c05f399011ca
Kevin Simpson <relasine@gmail.com>
2019-01-18 09:06:35 -0700

    Update App location in file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
+
+import Header from "../Header/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hi</h1>
+        <Header />
       </div>
     );
   }
