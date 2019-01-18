51978d2e23596203eed987dd444758f2ed2bd593
Kevin Simpson <relasine@gmail.com>
2019-01-18 12:20:16 -0700

    Updates

---
+++ precache-manifest.567588bfa44b40a8a4212f98fac1bb07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ca26eb79483042513c4",
+    "url": "/relasine/github.io/january-portfolio/static/css/main.fb317268.chunk.css"
+  },
+  {
+    "revision": "9ca26eb79483042513c4",
+    "url": "/relasine/github.io/january-portfolio/static/js/main.9ca26eb7.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/relasine/github.io/january-portfolio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "266ad5e19ebd2e972b98",
+    "url": "/relasine/github.io/january-portfolio/static/js/runtime~main.266ad5e1.js"
+  },
+  {
+    "revision": "b083a2d108ddb31447a94d160e04ad96",
+    "url": "/relasine/github.io/january-portfolio/static/media/clean-desk.b083a2d1.jpg"
+  },
+  {
+    "revision": "a464bdfa234cf4aa25cc765522174bd3",
+    "url": "/relasine/github.io/january-portfolio/index.html"
+  }
+];
