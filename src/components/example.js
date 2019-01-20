b25adb73aa7fa4873f5909c6d49c32fba165bfc6
Kevin Simpson <relasine@gmail.com>
2019-01-19 20:04:56 -0700

    Add second blog

---
+++ src/components/example.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import '../MyComponent.css'
+
+class MyComponent extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      data: [],
+      loading: false,
+      error: false
+    }
+  }
+
+
+
+  render() {
+    const list = 
+    return (
+      <ul>
+        {list}
+      </ul>
+    )
+  }
+}
--- src/content/blogs.js
+++ src/content/blogs.js
@@ -5,5 +5,12 @@ export const blogs = [
     imagePath: "./images/animation-article.png",
     link:
       "https://medium.com/@relasine/16-bit-sprite-animation-in-css-9cf8f36550d9"
+  },
+  {
+    id: "blog 2",
+    title: "Things I Wish I Understood When I Started with React.js",
+    imagePath: "./images/react-article.png",
+    link:
+      "https://medium.com/@relasine/things-i-wish-i-understood-when-i-started-with-react-js-d20b1cef7f81"
   }
 ];
