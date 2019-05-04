f40ef2c5a30f00166d5e9ee8e9f4b0b40a56cb94
Kevin Simpson <relasine@gmail.com>
2019-05-04 14:17:23 -0600

    Update about me

--- src/content/about.js
+++ src/content/about.js
@@ -1,5 +1,5 @@
 export const about = [
-  "I'm Kevin Simpson, a front end developer living in the north Denver metro area. I am a tenacious problem solver with a passion for React, CSS, unit testing, and teaching.",
+  "I'm Kevin Simpson, a front end developer living in the north Denver metro area and current a front end/JavaScript developer with Trimble SketchUp in Boulder. I am a tenacious problem solver with a passion for React, CSS, unit testing, and teaching.",
   "I push myself to learn something new about programming every day, and I take criticism with humility and grace.",
   " In my free time I am a husband and father who enjoys chasing my son around the house, Mario Kart marathons, and 90s-era JRPGs."
 ];
