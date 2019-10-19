6cb2e3f044a64ee65f1dcb2f4f5d45605d0627e3
Kevin Simpson <relasine@gmail.com>
2019-10-19 11:32:21 -0600

    Add MYM

--- src/content/about.js
+++ src/content/about.js
@@ -1,5 +1,5 @@
 export const about = [
   "I'm Kevin Simpson, a front end/JavaScript developer working for Trimble SketchUp in Boulder, CO. I am a tenacious problem solver with a passion for React, CSS, unit testing, and teaching.",
   "I push myself to learn something new about programming every day, and I take criticism with humility and grace.",
-  " In my free time I am a husband and father who enjoys chasing my son around the house, Mario Kart marathons, and 90s-era JRPGs."
+  " In my free time I am a husband and father who enjoys chasing my son around the house, building mechanical keyboards, and 90s-era JRPGs."
 ];
